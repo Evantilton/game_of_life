9795d5666a5f2a78f1379624fe39b82cbe398317
evantilton <evantilton@gmail.com>
2020-04-27 05:54:40 -0500

    made a grid of 0s and 1s that represent the board

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function make2dArray(cols, rows) {
-
+function make2dArray(rows, cols) {
+    let arr = new Array(cols);
+    for (let i = o; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
 }
 
 let grid;
+let cols = 10;
+let rows = 10;
 
 function setup() {
 
     grid = make2dArray(10, 10);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2)); 
+        }
+    }
 }
